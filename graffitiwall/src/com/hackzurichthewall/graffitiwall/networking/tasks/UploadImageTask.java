73aa79ff10a41b52820f4a5566a0ee9ded8c811a
Johannes Glöckle <johannes-gloeckle@t-online.de>
2014-10-11 20:29:07 +0200

    Added some .jar that were missing and added comments.

--- graffitiwall/src/com/hackzurichthewall/graffitiwall/networking/tasks/UploadImageTask.java
+++ graffitiwall/src/com/hackzurichthewall/graffitiwall/networking/tasks/UploadImageTask.java
@@ -23,10 +23,22 @@ import android.os.AsyncTask;
 import com.hackzurichthewall.graffitiwall.networking.RestClient;
 import com.hackzurichthewall.model.PictureComment;
 
+
+/**
+ * Task that uploads an image to the cloud storage. Uses HTTP POST with multipart
+ * @author johannes
+ *
+ */
 public class UploadImageTask extends AsyncTask<String, Void, Void>{
 
+	// interface that will do the upload of the fitting post
 	private AsyncResponse delegate = null;
 	
+	
+	/**
+	 * Always needs a class implementing the interface to be able to upload the post afterwards.
+	 * @param delegate class implementing the interface
+	 */
 	public UploadImageTask(AsyncResponse delegate) {
 		this.delegate = delegate;
 	}
@@ -34,12 +46,15 @@ public class UploadImageTask extends AsyncTask<String, Void, Void>{
 	@Override
 	protected Void doInBackground(String... params) {
 		
-		if (params == null || params.length == 0) {
+		if (params == null || params.length == 0) { // checking validity
 			throw new IllegalArgumentException("There was no bitmap to upload.");
 		}
+		
+		
 		// instantiates httpclient to make request
 	    DefaultHttpClient httpclient = new DefaultHttpClient();
 		
+	    // adding a post request
 		HttpPost post = new HttpPost(RestClient.URL + "/photos" + RestClient.API_KEY);
 		post.setHeader("Authorization", "Basic cGhpbG9ybGFuZG85MkB5YWhvby5kZToxMjM0");
 		post.setHeader("Accept", "application/json");
@@ -51,13 +66,14 @@ public class UploadImageTask extends AsyncTask<String, Void, Void>{
 		post.setEntity(multipart);
 
 		//Handles what is returned from the page 
-	    ResponseHandler responseHandler = new BasicResponseHandler() {
+	    ResponseHandler<?> responseHandler = new BasicResponseHandler() {
 
 			@Override
 			public String handleResponse(HttpResponse response)
 					throws HttpResponseException, IOException {
 					JSONObject respObject = null;
 					try {
+						// gets the JSON document with the URL
 						respObject = new JSONObject(EntityUtils.toString(response.getEntity()));
 					} catch (ParseException | JSONException e) {
 						// TODO Auto-generated catch block
@@ -69,11 +85,13 @@ public class UploadImageTask extends AsyncTask<String, Void, Void>{
 			            	String imageUrl = respObject.getString("url");
 			            	if (imageUrl != null) {
 			            		
+			            		// creating the comment with URL
 			            		PictureComment pComment = new PictureComment();
 			            		pComment.setmImageUrl(imageUrl);
 			            		JSONObject pCommentJSON = pComment.toJSON();
 			            		
 			            		if (delegate != null) {
+			            			// finally starting the post upload
 			            			delegate.uploadPost(pCommentJSON);
 			            		}
 			            		
@@ -101,6 +119,12 @@ public class UploadImageTask extends AsyncTask<String, Void, Void>{
 	    return null;
 	}
 	
+	
+	/**
+	 * Interface that just provides one method to upload a post.
+	 * 
+	 * @author Johannes Glöckle
+	 */
 	public interface AsyncResponse {
 		public void uploadPost(JSONObject post);
 	}
